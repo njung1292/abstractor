642755ca5802617ce7f6fea98226fbff1a8b247d
Noelle Jung <njjung@cmu.edu>
2013-01-26 10:25:19 -0500

    another update to ViewMode.js

--- js/ViewMode.js
+++ js/ViewMode.js
@@ -1,5 +1,5 @@
 //Constants//
-var open = 0;
+var open = -1;
 var closed = 1;
 
 var ViewMode = function(config) {
@@ -15,16 +15,24 @@ var ViewMode = function(config) {
 Box.clicked = closed;
 
 //Expand or collapse on click//
-function exp_collapse(){
-	Box.child_array = this.get_array();
-	for (var i = 0; i<Box.child_array.length; i++){
-		var id = Box.child_array[i].get_id(); //get the ID for the child box//
-		$("#"+id).click(function(){ 
-			if (Box.clicked == closed) $(this).slideDown("slow");
-			else $(this).slideUp("slow");
+Box.prototype.exp_collapse = function(){
+	var child_array = this.get_array();
+	for (var i = 0; i<child_array.length; i++){
+		var abst_id = child_array[i].get_id(); //get the ID for the child box//
+		var link_id = child_array[i].get_link_id(); //get the ID for the link//
+		var thisBox = this;
+		$("#"+abst_id).click(function(){
+			if (thisBox.clicked == closed) {
+				$(this).slideDown("slow"); //expand the child box//
+				$("#"+link_id).fadeIn(500); //fade in the link//
+			}
+			else {
+				$(this).slideUp("slow"); //collapse the child box//
+				$("#"+link_id).fadeOut(500); //fade out the link//
+			}
 		});
 	}
-	Box.clicked = -Box.clicked;
+	this.clicked = -this.clicked; //toggle back//
 }
 
 
